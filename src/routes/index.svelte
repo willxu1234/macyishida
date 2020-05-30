90a713a6da428bfd1acdada7837249c1d0e78912
Will Xu <willxu@wills-air.home>
2020-05-29 23:04:20 -0700

    redesign 1

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,35 @@
 <style>
+  h1 {
+    font-family: 'Palatino', 'Segoe UI', serif;
+    font-size: 4em;
+  }
+
+  h2 {
+    font-family: 'Palatino', 'Segoe UI', serif;
+    font-size: 1em;
+    color: gray;
+  }
+  
+  p {
+    font-family: 'Roboto', sans-serif;
+    font-size: 0.8em;
+  }
+
+  #container {
+    width: 70%;
+    height: 100vh;
+    margin: auto;
+    text-align: center;
+  }
+
+  #inner_container {
+    margin: 0;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+  }
 
 </style>
 
@@ -6,6 +37,16 @@
   <title>Macy Ishida</title>
 </svelte:head>
 
-<h1>Macy Ishida</h1>
-<p>I went to Jeju Island.</p>
-<img src="great-success.png" alt="jeju island" width="90%" />
+<div id='container'>
+  <div id='inner_container'>
+    <h1>Macy Ishida</h1>
+    <hr>
+    <h2>"may-see ee-shee-duh"</h2>
+    <p>
+    <a href='https://soundcloud.com/macy-ishida'>soundcloud</a> · 
+    <a href='https://www.pinterest.com/mayyshee/'>pinterest</a> ·
+    <a href='https://www.hudl.com/profile/7116400/macy-ishida'>hudl</a> ·
+    <a href='https://www.urbandictionary.com/define.php?term=Macy%20Ishida'>definition</a>
+    </p>
+  </div>
+</div>
